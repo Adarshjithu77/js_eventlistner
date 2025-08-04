6256c9ebfb2820d0e5ab40c933cb43878b2509e8
Adarshjithu77 <adarshjithu77@gmail.com>
2025-08-04 13:23:46 +0530

    assync and await

--- object.js
+++ object.js
@@ -149,27 +149,262 @@
 //     console.log("printing hello")
 // },1000)
 
-console.log("start")
-function abc(){
-    setTimeout(() =>{
-        console.log("inside the outer set tiimeout");
-        setTimeout(()=>{
-            console.log("inside the inner settimeout");
-            setTimeout(()=>{
-                console.log("inner the innermost settimeout");
-                setTimeout(()=>{
-                    console.log("outer the set timeout")
-                },1000)
-            },2000)
-        },3000)
+// console.log("start")
+// function abc(){
+//     setTimeout(() =>{
+//         console.log("inside the outer set tiimeout");
+//         setTimeout(()=>{
+//             console.log("inside the inner settimeout");
+//             setTimeout(()=>{
+//                 console.log("inner the innermost settimeout");
+//                 setTimeout(()=>{
+//                     console.log("outer the set timeout")
+//                 },1000)
+//             },2000)
+//         },3000)
 
-    },4000)
+//     },4000)
+// }
+
+// console.log("end")
+
+
+//promise
+
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise success");
+//     },1000);
+// })
+
+// promise.then((result)=>{
+//     console.log(result);
+// })
+
+// console.log(promise)
+
+
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve(5);
+//     },1000);
+// })
+
+// promise.then((result)=>{
+//     console.log(result);
+//     return result * 2;
+// }).then ((result)=>{
+//     console.log(result);
+//     return result*3;
+// }).then((value)=>{
+//     console.log(value);
+// })
+
+// console.log(promise)
+
+
+
+
+// const isprojectdone = new Promise((resolve,reject)=>{
+//     let projectDone =true;
+//     if(projectDone){
+//         resolve("projectDone by adarsh");
+//     }else{
+//         reject("project is not done by adarsh");
+//     }
+// });
+
+
+// isprojectdone
+// .then((result)=>{
+//     console.log(result);
+//     return "project done by adarsh and next task given";
+// }).then((result)=>{
+//     console.log(result)
+//     return "second task also done by adarsh"
+// }).then((result)=>{
+//     console.log(result)
+// })
+// .catch((error)=>{
+//     console.log(error);
+// });
+
+
+
+ 
+//promise all
+
+
+// let promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 1000)
+// })
+
+// let promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 1000)
+// })
+
+// let promise3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 1000)
+// })
+
+// Promise.all([promise1, promise2, promise3])
+//     .then((values) => {
+//         console.log(values)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+
+// //promise race
+
+
+// let promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise usucess')
+//     }, 1000)
+// })
+
+// let promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 100)
+// })
+
+// let promise3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 1000)
+// })
+
+// Promise.race([promise1, promise2, promise3])
+//     .then((values) => {
+//         console.log(values)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+
+//all setteled
+
+// let promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise usucess')
+//     }, 1000)
+// })
+
+// let promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 100)
+// })
+
+// let promise3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject('promise unsucess')
+//     }, 1000)
+// })
+
+// Promise.allSettled([promise1, promise2, promise3])
+//     .then((values) => {
+//         console.log(values)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+
+
+
+
+//promise any
+
+
+// let promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise usucess')
+//     }, 1000)
+// })
+
+// let promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('promise sucess')
+//     }, 100)
+// })
+
+// let promise3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject('promise unsucess')
+//     }, 1000)
+// })
+
+// Promise.any([promise1, promise2, promise3])
+//     .then((values) => {
+//         console.log(values)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+
+//assync and await
+
+
+// function CreateDelay() {
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res("success")
+//         },3000);
+//     });
+// }
+
+// CreateDelay();
+
+
+// function printmessage(){
+//     CreateDelay().then((value)=>{
+//         console.log(value);
+
+//     });
+// }
+// // printmessage();
+
+
+// async function demo(){
+//     console.log("hello 1");
+//     console.log(await CreateDelay());
+//     console.log("hellio")
+// }
+// demo();
+
+
+
+//dummyjson
+
+// function create(){
+//     fetch('https://dummyjson.com/products/search?q=phone')
+// .then(res => res.json())
+// .then(console.log);
+// }
+// create();
+
+
+const obj={
+    name:"synnefo",
+    numer:9952585633
 }
 
-console.log("end")
+const stringfiedDAta=JSON.stringify(obj)
 
+console.log(stringfiedDAta)
 
+const parsedData=JSON.parse(stringfiedDAta)
 
-
-
-
+console.log(parsedData)
